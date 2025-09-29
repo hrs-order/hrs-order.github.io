b29b07008554c990012c79f413138b14dfdbdae3
hrs-order <147409007+hrs-order@users.noreply.github.com>
2025-09-29 10:56:30 +0700

    Update authenService.js

--- app/services/authenService.js
+++ app/services/authenService.js
@@ -49,7 +49,7 @@
                 },
                 // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
                 signInFlow: 'popup',
-                signInSuccessUrl: "/sprint",
+                signInSuccessUrl: "/order",
                 signInOptions: [
                     // Leave the lines as is for the providers you want to offer your users.
                     // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
